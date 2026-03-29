ccf4a3eac281607e4d8c853d04e4702400fcd703
amixiid1 <amixiid0@gmail.com>
2026-03-29 12:21:50 +0300

    modify one line

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 const userData = [
   { id: 1, username: "abdilaahi", displayName: "abdilaahi" },
